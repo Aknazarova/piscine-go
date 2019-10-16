75a037725cb6c420ebd316bd409ef1ca2903fd2f
Your Name <you@example.com>
2019-10-16 22:44:48 +0600

    pcc

--- printcomb.go
+++ printcomb.go
@@ -12,8 +12,8 @@ func PrintComb() {
 					z01.PrintRune(y)
 					z01.PrintRune(z)
 					if x != '7' || y != '8' || z != '9' {
-						z01.PrintRune(32)
 						z01.PrintRune(44)
+						z01.PrintRune(32)
 					}
 				}
 			}
