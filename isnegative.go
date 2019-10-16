53409149a90870cda4bd5e9b52bb647ee486dc50
Your Name <you@example.com>
2019-10-16 19:20:11 +0600

    47fe

--- isnegative.go
+++ isnegative.go
@@ -1,5 +1,12 @@
 package piscine
 
-func IsNegative(nb int) {
+import "github.com/01-edu/z01"
 
-}
+func IsNegative(nb int) {
+if nb <= 0 {
+	z01.PrintRune(T)
+} else {
+	z01.PrintRune(F)
+ }
+  z01.PrintRune(10)
+
