d7212580b8605785a36bd64d755dff94dd572732
Your Name <you@example.com>
2019-10-16 22:40:43 +0600

    pc

--- printcomb.go
+++ printcomb.go
@@ -1,28 +1,23 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 //PrintRune PrintComb
-
 func PrintComb() {
-	for a := '0'; a <= '7'; a++ {
-		for b := '1'; b <= '8'; b++ {
-			for c := '2'; c <= '9'; c++ {
-				if a < b && b < c {
-					if a == '7' && b =='8' && c == '9'{
-					    z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(c)
-					}else{
-						z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(c)
-						z01.PrintRune(',')
+	for x := '0'; x <= '9'; x++ {
+		for y := x + 1; y <= '9'; y++ {
+			for z := y + 1; z <= '9'; z++ {
+				if x < y && y < z {
+					z01.PrintRune(x)
+					z01.PrintRune(y)
+					z01.PrintRune(z)
+					if x != '7' || y != '8' || z != '9' {
+						z01.PrintRune(32)
+						z01.PrintRune(44)
+					}
 				}
 			}
 		}
 	}
-	z01.PrintRune(10)
+	z01.PrintRune('\n')
 }
