7b76bef3e664f6be447109d35baf3af6abf176cf
Aknazarova <aknazarova_a@bi.group>
2019-10-21 18:38:48 +0600

    p01

---
+++ p01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func IterativeFactorial(nb int) int {
+
+	if nb >= 0 {
+		result := 1
+		for i := 2; i <= nb; i++{
+		result = result * i
+		}
+		return result
+	} else {
+	return 0
+	}
+}
