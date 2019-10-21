c0e7de4863a57b3088f1b878743bd29e2ef3678c
Aknazarova <aknazarova_a@bi.group>
2019-10-21 21:02:16 +0600

    1234

--- sqrt.go
+++ sqrt.go
@@ -3,10 +3,10 @@ package piscine
 func Sqrt(nb int) int {
 	result := 0
 	for i := 2; i < nb; i++ {
-	if nb%i == 0 {
-		result = i
-		break
+		if nb%i == 0 {
+			result = i
+			break
+		}
 	}
-}
 	return result
 }
