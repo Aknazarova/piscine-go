86669621df12c3ab0e1c79a8ee5ab1198ca8612a
Aknazarova <aknazarova_a@bi.group>
2019-10-21 19:09:13 +0600

    ert

--- test/test.go
+++ test/test.go
@@ -2,20 +2,10 @@ package main
 
 import (
 	"fmt"
-
 	piscine ".."
 )
 
 func main() {
-	s := "12345"
-	s2 := "0000000012345"
-	s3 := "000000"
-
-	n := piscine.BasicAtoi(s)
-	n2 := piscine.BasicAtoi(s2)
-	n3 := piscine.BasicAtoi(s3)
-
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
+	arg := 4
+	fmt.Println(piscine.IterativeFactorial(arg))
 }
