2d917ee3c807cefe113805fb53798fd5545d5449
Aknazarova <aknazarova_a@bi.group>
2019-10-21 20:20:08 +0600

    v11

--- iterativepower.go
+++ iterativepower.go
@@ -6,8 +6,9 @@ func IterativePower(nb int, power int) int {
 	}
 	result := 1
 	if power > 0 {
-		for i := 1; i <= power; i++ {
+		for i := 0; i < power; i++ {
 			result = result * nb
 		}
+	}
 	return result
 }
