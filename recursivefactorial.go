d58ee0ee7822e61b3151a6c6cc40b9a6d77e237f
Aknazarova <aknazarova_a@bi.group>
2019-10-21 19:48:36 +0600

    v

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,12 +5,10 @@ func RecursiveFactorial(nb int) int {
 		return 1
 	}
 	if nb < 0 {
-		return 0 
+		return 0
 	}
 	if nb > 1 && nb < 20 {
-		return nb* RecursiveFactorial(nb - 1)
+		return nb * RecursiveFactorial(nb-1)
 	}
 	return 0
 }
-
-
