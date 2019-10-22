c79efbd18d036fbbecbd8631972422e7008b3c32
Aknazarova <aknazarova_a@bi.group>
2019-10-22 19:42:31 +0600

    sad

--- alphacount.go
+++ alphacount.go
@@ -2,9 +2,8 @@ package piscine
 
 func AlphaCount(str string) int {
 	count := 0
-
-	for _, a := range str {
-		if (a > 'A' && a < 'Z') || (a > 'a' && a < 'z') {
+	for _, x := range str {
+		if (x > 'A' && x < 'Z') || (x > 'a' && x < 'z') {
 			count++
 		}
 	}
