ea90cfbe9e306c26536b9ec8349d3545736b117f
Aknazarova <aknazarova_a@bi.group>
2019-10-22 19:57:04 +0600

    ddd

---
+++ printnbrinorder.go
@@ -0,0 +1,12 @@
+package piscine
+
+func PrintNbrInOrder(n int)int{
+	result := 0
+	for n > 0 {
+		remainder := n % 10
+		result = result * 10
+		result = result + remainder
+		n = n / 10
+	}
+	return result
+}
