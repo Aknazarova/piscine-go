f10096c875e6fd80dd3249d68c5a6f53c5f3bec4
Aknazarova <aknazarova_a@bi.group>
2019-10-22 21:17:33 +0600

    ret

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -6,13 +6,13 @@ func PrintNbrInOrder(n int) {
 	s := string(n%10 + 48)
 	for n >= 10 {
 		n = n / 10
-		s = s + string(n%10 + 48)
+		s = s + string(n%10+48)
 	}
- 	for i := '0'; i <= '9'; i++ {
-		for _, num := range s { 
-			if i == num { 
+	for i := '0'; i <= '9'; i++ {
+		for _, num := range s {
+			if i == num {
 				z01.PrintRune(num)
-			}	
-		} 
+			}
+		}
 	}
 }
