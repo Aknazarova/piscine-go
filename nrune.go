64e50e498df1635dcc110abe95aa47fe7bbcac19
Aknazarova <aknazarova_a@bi.group>
2019-10-22 23:30:27 +0600

    wow

--- nrune.go
+++ nrune.go
@@ -1,10 +1,11 @@
 package piscine
-	func NRune(s string, n int) rune {
-		var x rune = 0
-		for index, char := range s {
-			if index == n-1 {
-				x = char
-			}
+
+func NRune(s string, n int) rune {
+	var x rune = 0
+	for index, char := range s {
+		if index == n-1 {
+			x = char
 		}
-		return x
 	}
+	return x
+}
