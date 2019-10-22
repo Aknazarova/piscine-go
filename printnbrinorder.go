e014b0d3e2786092edf9b2a3ee88c5aa932b93ef
Aknazarova <aknazarova_a@bi.group>
2019-10-22 21:14:45 +0600

    ddd

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,12 +1,18 @@
 package piscine
 
-func PrintNbrInOrder(n int) int {
-	result := 0
-	for n > 0 {
-		remainder := n % 10
-		result = result * 10
-		result = result + remainder
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	s := string(n%10 + 48)
+	for n >= 10 {
 		n = n / 10
+		s = s + string(n%10 + 48)
 	}
-	return result
+ 	for i := '0'; i <= '9'; i++ {
+		for _, num := range s { 
+			if i == num { 
+				z01.PrintRune(num)
+			}	
+		} 
+	}
 }
