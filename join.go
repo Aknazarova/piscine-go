95d3c22b53bc5dadfde65bf41797fc1f80eb47fe
Aknazarova <aknazarova_a@bi.group>
2019-10-23 20:49:56 +0600

    joy

--- join.go
+++ join.go
@@ -2,8 +2,15 @@ package piscine
 
 func Join(strs []string, sep string) string {
 	var a string
+	var count int = 0
+	for  range strs {
+		count++
+	} 
 	for b := range strs {
-		a += strs[b] + sep
+		a += strs[b]
+		if b != count-1 {// if it is last word
+			a += sep// print ':'
+		}
 	}
 	return a
 }
