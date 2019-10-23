1be3d0f86205b030c753c35d832c7734a49c4f1a
Aknazarova <aknazarova_a@bi.group>
2019-10-23 18:49:14 +0600

    fear

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(str string) bool {
 	for _, char := range str {
-		if! (char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' || char >= '0' && char <= '9') {
+		if !(char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' || char >= '0' && char <= '9') {
 			return false
 		}
 	}
