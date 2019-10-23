610a726983ad3ddf92b593b0be6bcec33ed8ced6
Aknazarova <aknazarova_a@bi.group>
2019-10-23 23:13:56 +0600

    cap

---
+++ printparams/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/01-edu/z01"
+import "os"
+
+func main(){
+	str := os.Args
+	count := 0
+	for range str {
+		count++
+		count := a
+	}
+	for i := a - 1; i > 0; i-- {
+		for _, char := range str[i] {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune(10)
+	}
+}
--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,7 +6,7 @@ import "os"
 func main() {
 	num := os.Args
 	for _, c := range num[0] {
-		z01.PrintRune(c)
+		z01.PrintRune(len(c))
 	}
 	z01.PrintRune(10)
 }
