67cf52653e6acac6d45f663f5e360f34ef0efefc
Aknazarova <aknazarova_a@bi.group>
2019-10-23 22:03:24 +0600

    cap

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,10 +1,11 @@
 package printprogramname
 
-import 	"github.com/01-edu/z01"
+import "github.com/01-edu/z01"
 import "os"
+
 func main(){
 	num := os.Args
-	for _, c := range num[0]{
+	for _, c := range num[0] {
 		z01.PrintRune(c)
 	}
 	z01.PrintRune(10)
