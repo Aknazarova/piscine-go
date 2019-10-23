d67caf6e1cf49c0dc1dc83b449f5c5b4f128129b
Aknazarova <aknazarova_a@bi.group>
2019-10-23 23:57:20 +0600

    i

---
+++ revparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/01-edu/z01"
+import "os"
+
+func main() {
+	str := os.Args
+	count := 0
+	for range str {
+		count++
+	}
+	for i := count - 1; i > 0; i-- { // ne scitat' 1-yi symbol
+		for _, char := range str[i] {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
