0c1dd5a7b52ece3ce54e1ece4307443fe319363f
Aknazarova <aknazarova_a@bi.group>
2019-10-23 19:01:31 +0600

    fea

---
+++ isprintable.go
@@ -0,0 +1,10 @@
+package piscine
+
+func IsPrintable(str string) bool {
+	for _, char := range str {
+		if !(char >= 'A' && char <= 'Z' || char >= 'a' && char <= 'z' ) {
+			return false
+		}
+	}
+	return true
+}
