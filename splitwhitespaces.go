b53201a6d206284aa8ab0778592294903b4983d7
Aigerim <aknazarova@github.com>
2019-10-24 21:41:30 +0600

    go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,7 +4,7 @@ func SplitWhiteSpaces(str string) []string {
 	a := make([]string, 1)
 	for _, x := range str {
 		if (x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z') {
-	a[0] = str
+			a[0] = str
 		}
 	}
 	return a
