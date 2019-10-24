73b6ca8b1f26986a07b34ba65775f9f974a4252c
Aknazarova <Aknazarova@github.com>
2019-10-24 17:49:05 +0600

    task7

---
+++ concatparams.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	str := ""
+	for index, char:= range args {
+		str += string(char)
+		if index != len(args) - 1 {
+			str +=  "\n"
+		}
+	}
+	return str
+}
