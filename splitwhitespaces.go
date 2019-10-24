4d2d7f4eb67b5a37e4a7f65b4b8b3640af97b458
Aigerim <aknazarova@github.com>
2019-10-24 21:40:43 +0600

    go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -2,6 +2,10 @@ package piscine
 
 func SplitWhiteSpaces(str string) []string {
 	a := make([]string, 1)
+	for _, x := range str {
+		if (x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z') {
 	a[0] = str
+		}
+	}
 	return a
 }
