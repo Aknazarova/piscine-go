2689f5cae5ab191711d4b70b509e8df7bdb9e471
Aknazarova <Aknazarova@github.com>
2019-10-24 17:50:28 +0600

    task7

--- concatparams.go
+++ concatparams.go
@@ -2,10 +2,10 @@ package piscine
 
 func ConcatParams(args []string) string {
 	str := ""
-	for index, char:= range args {
+	for index, char := range args {
 		str += string(char)
-		if index != len(args) - 1 {
-			str +=  "\n"
+		if index != len(args)-1 {
+			str += "\n"
 		}
 	}
 	return str
