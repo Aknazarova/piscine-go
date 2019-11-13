57119b6505fe2d5ad92c7d6f7bda15c22da13a5d
Aigerim <52777709+Aknazarova@users.noreply.github.com>
2019-11-13 20:36:35 +0600

    Update basicatoi.go

--- quest-03/basicatoi.go
+++ quest-03/basicatoi.go
@@ -1,13 +1,29 @@
-package piscine
+package main
+
+import "fmt"
+
+func BasicAtoi(s string) int {
+	x := 0
+	for _, n := range s {
+		y := 0
+		for i := '1'; i <= n; i++ {
+			y++
+		}
+		x = x*10 + y
+	}
+	return x
+}
 
 func main() {
+	s := "12345"
+	s2 := "0000000012345"
+	s3 := "000000"
 
-	s := "0000000012345"
+	n := BasicAtoi(s)
+	n2 := BasicAtoi(s2)
+	n3 := BasicAtoi(s3)
 
-	/** converting the s variable into an int using Atoi method */
-	n, err := BasicAtoi(s)
-	if err == nil {
-		fmt.PrintRune(n)
-	}
-
+	fmt.Println(n)
+	fmt.Println(n2)
+	fmt.Println(n3)
 }
